213573eacb8ec54b497d0644c61ba8c9070b1854
unknown <anton8004@students.amikom.ac.id>
2018-08-14 11:44:38 +0700

    Update Database

--- db/databaseku.sql
+++ db/databaseku.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 13 Agu 2018 pada 12.14
+-- Generation Time: 14 Agu 2018 pada 11.42
 -- Versi Server: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `t_admin` (
-`id_admin` tinyint(2) NOT NULL,
+`id_admin` int(11) NOT NULL,
   `username` varchar(35) NOT NULL,
   `password` varchar(255) NOT NULL,
   `level` enum('admin','superadmin') NOT NULL,
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `t_admin` (
 
 INSERT INTO `t_admin` (`id_admin`, `username`, `password`, `level`, `email`, `status_login`, `status`) VALUES
 (1, 'Super Admin', '$2y$10$NwTSANgtnU1MNSaR/tVK0u4pDv/fW74QYdrcl6MC2Ljs.oHMaTTMW', 'superadmin', 'superadmin@gmail.com', '1', '2'),
-(2, 'Admin', '$2y$10$NwTSANgtnU1MNSaR/tVK0u4pDv/fW74QYdrcl6MC2Ljs.oHMaTTMW', 'admin', 'admin@gmail.com', '1', '1'),
+(2, 'Admin', '$2y$10$NwTSANgtnU1MNSaR/tVK0u4pDv/fW74QYdrcl6MC2Ljs.oHMaTTMW', 'admin', 'admin@gmail.com', '1', '2'),
 (3, 'admin2', '$2y$10$s7EVjaJtDW1mr8qbUniHYe9zQcvRcKHeVPGN.veX4qu2a2Q1H0lkW', 'admin', 'admin2@gmail.com', '1', '1'),
 (4, 'admin3', '$2y$10$DNL/HoUYIfE6KrMgQ4L2N.Ir1DZ6sm5fL.pEkzoNGeS/UUbixVMi.', 'admin', 'admin3@gmail.com', '1', '2');
 
@@ -53,12 +53,12 @@ INSERT INTO `t_admin` (`id_admin`, `username`, `password`, `level`, `email`, `st
 --
 
 CREATE TABLE IF NOT EXISTS `t_dp` (
-`id_dp` tinyint(5) NOT NULL,
+`id_dp` int(11) NOT NULL,
   `username` varchar(35) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `status` enum('1','2') NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data untuk tabel `t_dp`
@@ -76,7 +76,7 @@ INSERT INTO `t_dp` (`id_dp`, `username`, `email`, `password`, `status`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `t_kurir` (
-`id_kurir` tinyint(5) NOT NULL,
+`id_kurir` int(11) NOT NULL,
   `username` varchar(35) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -121,23 +121,48 @@ INSERT INTO `t_member` (`id_member`, `username`, `fullname`, `email`, `password`
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `t_transaksi`
+--
+
+CREATE TABLE IF NOT EXISTS `t_transaksi` (
+`id_transaksi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `tgl_pengiriman` date NOT NULL,
+  `no_resi` varchar(255) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `kode_pos` int(5) NOT NULL,
+  `no_tlp` varchar(35) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data untuk tabel `t_transaksi`
+--
+
+INSERT INTO `t_transaksi` (`id_transaksi`, `id_user`, `tgl_pengiriman`, `no_resi`, `nama`, `alamat`, `kode_pos`, `no_tlp`) VALUES
+(2, 1, '2018-08-14', 'RES140820181', 'Tri Winanto', 'Jalan Sepakbola No 120, Condongcatur, Depok, Sleman, Ngropoh, Condongcatur, Kec. Depok, Jogja, Daerah Istimewa Yogyakarta ', 55283, '0899541311');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `t_user`
 --
 
 CREATE TABLE IF NOT EXISTS `t_user` (
-`id_user` tinyint(5) NOT NULL,
+`id_user` int(11) NOT NULL,
   `username` varchar(35) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `status` enum('1','2') NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data untuk tabel `t_user`
 --
 
 INSERT INTO `t_user` (`id_user`, `username`, `email`, `password`, `status`) VALUES
-(1, 'anton', 'ateha@gmail.com', '$2y$10$MQbuDf105ZsKMlsC32c2nukwIf9cGsXlvmnSeYWoxk6CkOFYSCjIK', '1');
+(1, 'anton', 'ateha@gmail.com', '$2y$10$MQbuDf105ZsKMlsC32c2nukwIf9cGsXlvmnSeYWoxk6CkOFYSCjIK', '2'),
+(2, 'Tri Winanto', 'tri@gmail.com', '$2y$10$8H4krasdEeDvpwlJta8a7.wPbNXTfBzL8Mkj2fIsX98xAmtZrRSmS', '2');
 
 --
 -- Indexes for dumped tables
@@ -168,6 +193,12 @@ ALTER TABLE `t_member`
  ADD PRIMARY KEY (`id_member`);
 
 --
+-- Indexes for table `t_transaksi`
+--
+ALTER TABLE `t_transaksi`
+ ADD PRIMARY KEY (`id_transaksi`);
+
+--
 -- Indexes for table `t_user`
 --
 ALTER TABLE `t_user`
@@ -181,27 +212,32 @@ ALTER TABLE `t_user`
 -- AUTO_INCREMENT for table `t_admin`
 --
 ALTER TABLE `t_admin`
-MODIFY `id_admin` tinyint(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `t_dp`
 --
 ALTER TABLE `t_dp`
-MODIFY `id_dp` tinyint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `id_dp` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `t_kurir`
 --
 ALTER TABLE `t_kurir`
-MODIFY `id_kurir` tinyint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `id_kurir` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `t_member`
 --
 ALTER TABLE `t_member`
 MODIFY `id_member` tinyint(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
 --
+-- AUTO_INCREMENT for table `t_transaksi`
+--
+ALTER TABLE `t_transaksi`
+MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `t_user`
 --
 ALTER TABLE `t_user`
-MODIFY `id_user` tinyint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
